130a426818bb9f5998ef808b92769dc2d2186630
witherScript <chandlerscott123@gmail.com>
2023-10-15 10:54:49 -0500

    update readme and SQL file: fix import error, include database and appsettings instructions

--- Genesis_Scott.sql
+++ Genesis_Scott.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.27, for macos11 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.27, for macos11 (arm64)
 --
 -- Host: localhost    Database: Genesis_Scott
 -- ------------------------------------------------------
@@ -7,13 +7,46 @@
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Clients`
+--
+
+DROP TABLE IF EXISTS `Clients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Clients` (
+  `ClientId` int NOT NULL AUTO_INCREMENT,
+  `EmployeeId` int DEFAULT NULL,
+  `Name` varchar(255) DEFAULT NULL,
+  `SpecialtyNeeded` int DEFAULT NULL,
+  PRIMARY KEY (`ClientId`),
+  KEY `EmployeeId` (`EmployeeId`),
+  CONSTRAINT `clients_ibfk_1` FOREIGN KEY (`EmployeeId`) REFERENCES `Employees` (`EmployeeId`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Employees`
+--
+
+DROP TABLE IF EXISTS `Employees`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Employees` (
+  `EmployeeId` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) DEFAULT NULL,
+  `Specialty` int DEFAULT NULL,
+  PRIMARY KEY (`EmployeeId`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -24,4 +57,4 @@
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-08 12:47:05
+-- Dump completed on 2023-10-15 10:43:20
--- README.md
+++ README.md
@@ -16,12 +16,26 @@ A web application that allows managers to manage their employees' details and ad
 1. Ensure .NET SDK and runtime are installed on your machine.
 2. Clone this repository to your local machine.
 3. Navigate to the HairSalon.Solution directory in your terminal.
-4. Run the command ```dotnet restore``` to install necessary packages.
-5. Run the command ```dotnet build``` to compile the application.
-6. Run ```dotnet run``` to start the server and application.
-7. Visit localhost:5000 in your browser to access the Employee Manage application.
-8. Follow on-screen prompts to manage employee details.
-9. If you come across any difficulties or wish to give feedback, don't hesitate to get in touch or raise an issue on the repository.
+4. Touch a file in the HairSalon.Solution directory called appsettings.json add the following code, replacing the uid and pwd values with your own username and password for MySQL.
+
+```bash
+$ touch appsettings.json
+```
+
+```csharp
+{
+  "ConnectionStrings": {
+      "DefaultConnection": "Server=localhost;Port=3306;database=Genesis_Scott;uid=[YOUR-USERNAME];pwd=[YOUR-MYSQL-PASSWORD];"
+  }
+}
+```
+5. Set up the Database. Follow the instructions in the LearnHowToProgram.com lesson ["Creating a Test Database: Exporting and Importing Databases with MySQL Workbench"](https://part-time.learnhowtoprogram.com/c-and-net/database-basics/creating-a-test-database-exporting-and-importing-databases-with-mysql-workbench) to use the Genesis_Scott.sql file located at the top level of this repo to create a new database in MySQL Workbench with the name Genesis_Scott.
+6. Run the command ```dotnet restore``` to install necessary packages.
+7. Run the command ```dotnet build``` to compile the application.
+8. Run ```dotnet run``` to start the server and application.
+9. Visit localhost:5000 in your browser to access the Employee Manage application.
+10. Follow on-screen prompts to manage employee details.
+11. If you come across any difficulties or wish to give feedback, don't hesitate to get in touch or raise an issue on the repository.
 
 ## Known Bugs
 - CSS styles might not be immediately applied because of caching challenges.
